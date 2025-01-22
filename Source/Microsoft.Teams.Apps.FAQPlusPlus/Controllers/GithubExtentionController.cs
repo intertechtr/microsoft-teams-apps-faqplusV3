f2b4b1b1559a2dfb878dba65ed41d52e26cb5569
gence <gence@users.noreply.github.com>
2025-01-22 18:36:44 +0300

    response fix

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
@@ -1,6 +1,10 @@
+using Azure.AI.OpenAI;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Teams.Apps.FAQPlusPlus.Models;
+using NuGet.Common;
+using System.Threading.Tasks;
 
 namespace Microsoft.Teams.Apps.FAQPlusPlus.Controllers
 {
@@ -16,14 +20,17 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Controllers
         }
 
         [HttpPost("agent")]
-        public IActionResult Agent([FromHeader(Name = "X-GitHub-Token")] string githubToken, [FromBody] CopilotData copilotData)
+        public async Task Agent([FromHeader(Name = "X-GitHub-Token")] string githubToken, [FromBody] CopilotData copilotData)
         {
             foreach (var message in copilotData.Messages)
             {
                 this.logger.LogInformation($"Role: {message.Role}, Content: {message.Content}");
             }
 
-            return Ok("{\"id\": \"1234567890\", \"model\": \"llama2-70b-chat\",  \"choices\": [    {      \"index\": 0,     \"finish_reason\": \"stop\",     \"message\": {        \"role\": \"assistant\",        \"content\": \"Hello World\"      }    }  ],  \"created\": 1234567890,  \"object\": \"chat.completion\",  \"usage\": {    \"prompt_tokens\": 205,    \"completion_tokens\": 5,    \"total_tokens\": 210  }}");
+            string responseString = "{\"message\": { \"role\": \"assistant\", \"content\": \"Hello World\" } }";
+
+            await this.Response.WriteAsync(responseString);
+            await this.Response.Body.FlushAsync();
         }
 
         [HttpGet("callback")]
