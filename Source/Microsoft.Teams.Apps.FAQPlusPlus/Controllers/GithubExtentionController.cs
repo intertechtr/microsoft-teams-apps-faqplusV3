5b325d9820c134b32f18617bcd76f5570781bb89
gence <gence@users.noreply.github.com>
2025-01-22 18:59:26 +0300

    response test

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Controllers
                 this.logger.LogInformation($"Role: {message.Role}, Content: {message.Content}");
             }
 
-            string responseString = "{\"message\": { \"role\": \"assistant\", \"content\": \"Hello World\" } }";
+            string responseString = "{\"id\":\"chatcmpl-123\",\"object\":\"chat.completion.chunk\",\"created\":1694268190,\"model\":\"gpt-3.5-turbo-0125\", \"system_fingerprint\": \"fp_44709d6fcb\", \"choices\":[{\"index\":0,\"delta\":{\"role\":\"assistant\",\"content\":\"Hello world.\"},\"logprobs\":null,\"finish_reason\":null}]}";
 
             await this.Response.WriteAsync(responseString);
             await this.Response.Body.FlushAsync();
