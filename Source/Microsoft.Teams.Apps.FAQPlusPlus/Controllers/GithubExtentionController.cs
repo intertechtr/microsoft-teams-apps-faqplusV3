55cfd46670b14823ab12f6a447325d221a9c26d6
Seliguzel <SALIHE@intertech.com.tr>
2025-02-10 12:09:34 +0300

    Update GithubExtentionController.cs

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Controllers/GithubExtentionController.cs
@@ -41,7 +41,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Controllers
             string unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
 
             /*string responseString = $"data: {{ \"object\":\"chat.completion.chunk\", \"type\":\"success\", \"choices\":[{{\"index\":0,\"delta\":{{\"role\":\"assistant\",\"content\":\"{JsonEncodedText.Encode(answer)}\"}}}}]}}\n\n";*/
-            string responseString = $"data: {{ \"id\":\"chtcmp-123\",\"object\":\"chat.completion.chunk\", \"system_fingerprint\": \"fp_46409d6sgb\", \"choices\":[{{\"index\":0,\"delta\":{{\"role\":\"assistant\",\"content\":\"{JsonEncodedText.Encode(answer)}\"}},\"logprobs\":null,\"finish_reason\":\"stop\"}}]}}\n\n";
+            string responseString = $"data: {{ \"id\":\"chtcmp-123\",\"object\":\"chat.completion.chunk\", \"system_fingerprint\": \"fp_46409d6sgb\", \"choices\":[{{\"index\":0,\"delta\":{{\"role\":\"assistant\",\"content\":\"{JsonEncodedText.Encode(answer)}\"}},\"logprobs\":null,\"finish_reason\":null}}]}}\n\n";
             this.logger.LogInformation($"Response at Github Extension Level: {responseString}");
 
             string doneString = "data: [DONE]\n\n";
